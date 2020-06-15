8b0a820bbdad1842cddebc5f5d56399c0db54379
Ryanalfarisi <ryanalfarisi28@gmail.com>
2020-06-16 01:02:23 +0700

    Finished

---
+++ precache-manifest.18e96f05ecf432e11762d2f5c34cad89.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6a1893d1f70e34c558",
+    "url": "/css/app.1d368fb9.css"
+  },
+  {
+    "revision": "91a01b3a6eb5b01592ec",
+    "url": "/css/chunk-vendors.7d1c4736.css"
+  },
+  {
+    "revision": "7edd05ce186814e9bd96892c83c0adcf",
+    "url": "/img/car_2.7edd05ce.png"
+  },
+  {
+    "revision": "ff5647d519ea1166bd4ed3acfaa5c093",
+    "url": "/img/maps.ff5647d5.png"
+  },
+  {
+    "revision": "682bee79d2e377c43581b4d57533f60c",
+    "url": "/img/mobil.682bee79.png"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/img/people.9acfe78b.png"
+  },
+  {
+    "revision": "adcf490c6d47e5cb789eeb01caa7765a",
+    "url": "/img/red_2.adcf490c.png"
+  },
+  {
+    "revision": "8958b4d406d97d91cfb0e02b78f206b9",
+    "url": "/img/red_3.8958b4d4.png"
+  },
+  {
+    "revision": "6c569d4bb923b5023a686b52437dd0f5",
+    "url": "/img/red_4.6c569d4b.png"
+  },
+  {
+    "revision": "58e3e58d45d1e5a1d76bbc32c59d613f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6a1893d1f70e34c558",
+    "url": "/js/app.e6616e28.js"
+  },
+  {
+    "revision": "91a01b3a6eb5b01592ec",
+    "url": "/js/chunk-vendors.a868259b.js"
+  },
+  {
+    "revision": "0ae7339eb9a779a262fb26159fc798da",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
